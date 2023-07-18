cceabd623b64bf16ac82322ff5443ae53f66309d
xlxuxs <ada896203@gmail.com>
2023-07-18 12:38:20 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stio.h>
+/*
+ *Write a program that prints _putchar, followed by a new line.
+ *The program should return 0
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
