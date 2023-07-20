6fbe50bd80c980fbf1a6bae380df8fe416ab3d0a
xlxuxs <ada896203@gmail.com>
2023-07-20 13:05:58 -0700

    function

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,7 @@
 *@a:an interger from main function to be checked
 *@b:an interger from main function to be checked
 *
-*return:addition of the multiplicattion
+*Return:addition of the multiplicattion
 */
 int mul(int a, int b)
 	{
