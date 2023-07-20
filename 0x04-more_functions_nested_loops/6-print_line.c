af8cb1d2ed8d52db7ac2ecd37e7c8323a313431e
xlxuxs <ada896203@gmail.com>
2023-07-20 09:29:26 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,14 @@
 *
 */
 void print_line(int n)
-	{
-	int i;
-	for (i = 0; i < n; i++)
-	{
+{
 	if (n <= 0)
 	{
 	_putchar('\n');
 	}
+	else
+	for (int i = 0; i < n; i++)
+	{
 	_putchar('_');
 	}
 	_putchar('\n');
